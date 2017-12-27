5185942f8fa69195336ba0a3f3a0234b7f95a593
erez <erezmoritz1@gmail.com>
2017-12-27 22:16:31 +0200

    add drItemizer implementation

--- itemizers/dr_itemizer.py
+++ itemizers/dr_itemizer.py
@@ -1,9 +1,70 @@
+import re
+
+from model.item import Item, Comment
+
+
 class DrItemizer():
-    def __init__(self):
-        self.items = []
+    def __init__(self, drive_client):
+        self.items = dict()
+        self.drive_client = drive_client
 
     def parse_message(self, message):
-        pass
+        text = message["text"]
+
+        dr_id = parse_dr_id(text)
+        if not dr_id:
+            return
+
+        if dr_id in self.items:
+            item = self.items[dr_id]
+
+            if item.first_mentioned > message.timestamp:
+                item.first_mentioned = message.timestamp
+
+            if item.last_mentioned < message.timestamp:
+                item.last_mentioned = message.timestamp
+
+            if message.is_bot and (item.last_mentioned_bot is None or item.last_mentioned_bot < message.timestamp):
+                item.last_mentioned_bot = message.timestamp
+        else:
+            item = Item()
+            item.id = dr_id
+            item.type = 'dr'
+            item.first_mentioned = message.timestamp
+            item.last_mentioned = message.timestamp
+            if message.is_bot:
+                item.last_mentioned_bot = message.timestamp
+            self.items[dr_id] = item
 
     def enrich(self):
-        pass
+
+        for key, item in self.items:
+            dr = self.drive_client.files().get(fileId=item.id, fields="owners, name").execute()
+            dr_comments = self.drive_client.comments().list(fileId=item.id, fields="comments").execute()['comments']
+
+            owner_name = dr['owners'][0]['displayName']
+            item.owner = dr['owners'][0]['emailAddress']
+            item.title = dr['name']
+
+            for comment in dr_comments:
+                if owner_name == comment['author']['displayName']:
+                    continue
+
+                com = Comment()
+                com.time = comment['createdTime']
+                com.owner = comment['author']['displayName']
+                com.text = comment['content']
+                item.comments.append(com)
+
+
+def parse_dr_id(text):
+    res = re.search('<https://docs.google(.*?)>', text)
+    if not res:
+        return None
+
+    res = re.search('/[-\w]{25,}/', res.group())
+    if not res:
+        return None
+
+    file_id = res.group().replace("/", "")
+    return file_id
--- itemizers/pr_itemizer.py
+++ itemizers/pr_itemizer.py
@@ -1,9 +1,5 @@
-from github import Github
-
 from model.item import Item
 from model.item import Comment
-
-import config
 
 
 class PrItemizer(object):
@@ -12,37 +8,34 @@ class PrItemizer(object):
         self.github_client = github_client
 
     def parse_message(self, message):
-
         text = message["text"]
 
-        if 'https://github.com/augurysys' not in text:
-            return
-
         pr_id = parse_pr_id(text)
+        if not pr_id:
+            return
 
         if pr_id in self.items:
             item = self.items[pr_id]
 
-            if item.first_mentioned > message.time:
-                item.first_mentioned = message.time
+            if item.first_mentioned > message.timestamp:
+                item.first_mentioned = message.timestamp
 
-            if item.last_mentioned < message.time:
-                item.last_mentioned = message.time
+            if item.last_mentioned < message.timestamp:
+                item.last_mentioned = message.timestamp
 
-            if message.is_bot and (item.last_mentioned_bot is None or item.last_mentioned_bot < message.time):
-                item.last_mentioned_bot = message.time
+            if message.is_bot and (item.last_mentioned_bot is None or item.last_mentioned_bot < message.timestamp):
+                item.last_mentioned_bot = message.timestamp
         else:
             item = Item()
             item.id = pr_id
             item.type = 'pr'
-            item.first_mentioned = message.time
-            item.last_mentioned = message.time
+            item.first_mentioned = message.timestamp
+            item.last_mentioned = message.timestamp
             if message.is_bot:
-                item.last_mentioned_bot = message.time
+                item.last_mentioned_bot = message.timestamp
             self.items[pr_id] = item
 
     def enrich(self):
-
         to_delete = []
 
         for key, item in self.items:
@@ -72,6 +65,8 @@ class PrItemizer(object):
 
 
 def parse_pr_id(message):
+    if 'https://github.com/augurysys' not in message:
+        return None
     items = message.split('/')
     index = items.index('augurysys')
     return '{}_{}'.format(items[index + 1], items[index + 3])
