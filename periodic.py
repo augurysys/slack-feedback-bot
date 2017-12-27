54b3ddbaf48e216ce98bede8a7e6c553698c179b
Amit Ziv-Kenet <amit.zivkenet@gmail.com>
2017-12-27 21:32:24 +0200

    fix config

--- periodic.py
+++ periodic.py
@@ -3,7 +3,7 @@ from itemizers import itemizer_factory
 from slack_scrapper import SlackScrapper
 
 if __name__ == "__main__":
-    for (channel_name, channel_type) in channels_config():
+    for (channel_name, channel_type) in channels_config:
         itemizer = itemizer_factory(channel_type)
         scrapper = SlackScrapper(channel_name, slack_token)
         scrapper.parse(itemizer)
