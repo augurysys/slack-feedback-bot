c493d3b57ae1731b1c84e57a136e3c7994282e46
Assaf Mizrachi <amizrachi@augury.com>
2017-12-28 14:33:06 +0200

    change gif

--- dispatch.py
+++ dispatch.py
@@ -13,17 +13,17 @@ def _format(item):
 
     if item_type == STALENESS_CRITERION:
         text = staleness_text.format(name=item[0].owner, url=item[0].url)
-        image = "https://media.giphy.com/media/3oFzmtbkFIVT3zb6hi/giphy.gif"
+        image = "https://media.giphy.com/media/3o752d889NJGsyWsUM/giphy.gif"
     elif item_type == HYPE_CRITERION:
         users = list(set(c.owner for c in item[0].comments))
         text = hype_text.format(names=", ".join(users), url=item[0].url)
-        image = "https://media.giphy.com/media/xULW8GA2epJ7O0AnEk/giphy.gif"
+        image = "https://media.giphy.com/media/xULW8EMvuylVNlSYEw/giphy.gif"
     elif item_type == HOT_COMMENT_CRITERION:
         commenter = item[1]['comment'].owner
         comment_text = item[1]['comment'].text
 
         text= hot_comment_text.format(name=commenter, comment=comment_text, title=item[0].title, url=item[0].url)
-        image = "https://media.giphy.com/media/3ohc16Cky5Mu9rZwT6/giphy.gif"
+        image = "https://media.giphy.com/media/xULW8NFsOvKVHiCgko/giphy.gif"
     else:
         return None
     
