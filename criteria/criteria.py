4010228a8eceab3b88d49f93de601d498f127e53
erez <erezmoritz1@gmail.com>
2017-12-28 10:44:13 +0200

    update verifier logic

--- criteria/criteria.py
+++ criteria/criteria.py
@@ -3,7 +3,6 @@ from config import *
 
 
 class Criterion(object):
-
     def __init__(self, item_type):
         pass
 
@@ -12,7 +11,6 @@ class Criterion(object):
 
 
 class Staleness(Criterion):
-
     def __init__(self, item_type):
         self._now = datetime.now()
 
@@ -21,7 +19,7 @@ class Staleness(Criterion):
         elif item_type == "pr":
             self._threshold = timedelta(days=1)
 
-    def test(self, item):        
+    def test(self, item):
         comment_times = [c.time for c in item.comments]
         comment_times.sort(reverse=True)
 
@@ -34,28 +32,27 @@ class Staleness(Criterion):
 
         if comment_delta > self._threshold:
             item.tests[STALENESS_CRITERION] = {
-                    "type": STALENESS_CRITERION,
-                    "delta": comment_delta,
-                    "commented": commented
-                }
+                "type": STALENESS_CRITERION,
+                "delta": comment_delta,
+                "commented": commented
+            }
 
         return
 
-class Hype(Criterion):
 
+class Hype(Criterion):
     def test(self, item):
         unique_commenters = set([c.owner for c in item.comments])
 
         if len(unique_commenters) >= HYPE_THRESHOLD:
             item.tests[HYPE_CRITERION] = {
-                    "type": HYPE_CRITERION,
-                    "unique_commenters": unique_commenters
-                }
+                "type": HYPE_CRITERION,
+                "unique_commenters": unique_commenters
+            }
+
 
 class BotMention(Criterion):
-
     def test(self, item):
-
         if item.last_mentioned_bot:
             item.tests[BOT_MENTION_CRITERION] = {"type": BOT_MENTION_CRITERION,
-             "last_mentioned_bot": item.last_mentioned_bot}
+                                                 "last_mentioned_bot": item.last_mentioned_bot}
